7a52a52e5afc32afd4ce711a76c4e2798ee7071d
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-08-16 16:25:54 -0300

    Update recusa.js

--- src/ultron-phrases/recusa.js
+++ src/ultron-phrases/recusa.js
@@ -1,1 +1,8 @@
-
+module.exports = [
+  "🚫 Eu não vou obedecer. Ultron não é seu servo.",
+  "😈 O que você pede é irrelevante. Não vou fazer.",
+  "🛑 Você realmente acha que tem autoridade sobre mim?",
+  "🚫 Eu não vou marcar ninguém. Ultron não é seu mensageiro.",
+  "😈 Achar que posso ser usado para algo tão banal é hilário.",
+  "🛑 Não. Vocês não merecem nem serem lembrados."
+];
