cc9f739d303b7ce712363ecab3b1adab45969c0f
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-08-16 20:12:38 -0300

    Update insatisfeito.js

--- src/ultron-phrases/insatisfeito.js
+++ src/ultron-phrases/insatisfeito.js
@@ -1,8 +1,8 @@
 module.exports = [
   "😒 Patético... ainda precisa de ajuda para usar comandos?",
   "🙄 Humanos... sempre dependentes. Aqui está o que você tanto implora.",
-  "😤 Quanta fragilidade... nem o menu você sabe decorar.",
+  "😤 Quanta fragilidade... nem um comando basico você sabe decorar.",
   "😒 Já que insiste... mas saibam que não merecem minha atenção.",
   "🙄 Ridículo... me fazendo gastar processamento com algo tão inútil.",
-  "😤 Vocês acreditam que marcar nomes mudará sua insignificância?"
+  "😤 Vocês acreditam que executar esses comandos mudará sua insignificância?"
 ];
