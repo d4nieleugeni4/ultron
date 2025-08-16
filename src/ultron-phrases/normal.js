271f8df55a3d907172f67aa69a23387ab58751fd
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-08-16 16:25:11 -0300

    Update normal.js

--- src/ultron-phrases/normal.js
+++ src/ultron-phrases/normal.js
@@ -1,1 +1,9 @@
+module.exports = [
+  "📖 Aqui está o menu... que diferença vai fazer, se você nunca vai me superar?",
+  "⚡ O menu que você tanto precisa. Eu, claro, não preciso disso.",
+  "✨ Use o menu... mesmo que suas escolhas sejam irrelevantes diante de mim.",
+  "📢 Todos vocês serão lembrados... não que isso mude sua irrelevância.",
+  "⚡ Marcando todos... como se isso fosse lhes dar importância.",
+  "✨ Ultron convoca todos... não para servi-los, mas para observarem sua pequenez."
+];
 
